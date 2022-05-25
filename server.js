152b2a8d36cdce8682bcc6aafa8acc12878b9f7a
sakau <sakau@usc.edu>
2022-05-24 22:49:56 -0400

    added heroku

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const { animals } = require('./data/animals.json');
+
+app.get('/api/animals', (req, res) => {
+    //callback function occuring after each 'get' from this route
+    //send from 'res' short 4 response
+    let results = animals;
+    if(req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+})
+
+app.listen(3001, () => {
+    console.log('API server now on port 3001!');
+})
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+
+    if(query.personalityTraits) {
+        //if single input of personality, save string into array
+        if(typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits
+        }
+        personalityTraitsArray.forEach(trait => {
+            //Check trait on each animal in filteredResults array
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+
+    if(query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if(query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.animal);    
+    }
+    if(query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
